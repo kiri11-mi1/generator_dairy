32e0d30fd7e319084194d6ae17db5933c1d02467
kiri11_mi1 <kirill.mil.0310@mail.ru>
2020-11-08 17:19:39 +0700

    Написал методы работы с карточками

--- bot.py
+++ bot.py
@@ -2,16 +2,16 @@ import requests
 
 
 class Bot:
-    '''Бот с базовым функционалом'''
+    '''Бота дла работы с Trello'''
 
     def __init__(self, token, key):
         self.api_token = token
         self.api_key = key
-        self.url = "https://api.trello.com/"
+        self.url = "https://api.trello.com/1/"
     
 
     def add_list(self, idBoard, name_list):
-        method = '1/lists/'
+        method = 'lists/'
         params = {
             'key': self.api_key,
             'token': self.api_token,
@@ -23,7 +23,7 @@ class Bot:
 
 
     def delete_list(self, id_list):
-        method = f'1/lists/{id_list}'
+        method = f'lists/{id_list}'
         params = {
             'key': self.api_key,
             'token': self.api_token,
@@ -34,7 +34,40 @@ class Bot:
     
 
     def get_lists(self, board_id):
-        method = f'1/boards/{board_id}/lists'
+        method = f'boards/{board_id}/lists'
+        params = {
+            'key': self.api_key,
+            'token': self.api_token,
+        }
+        response = requests.get(self.url+method, params=params)
+        return response
+    
+
+    def create_card(self, list_id, name_card):
+        method = 'cards'
+        params = {
+            'key': self.api_key,
+            'token': self.api_token,
+            'idList': list_id,
+            'name': name_card
+        }
+        response = requests.post(self.url+method, params=params)
+        return response
+
+
+    def add_comment(self, id_card, text):
+        method = f'cards/{id_card}/actions/comments'
+        params = {
+            'key': self.api_key,
+            'token': self.api_token,
+            'text': text
+        }
+        response = requests.post(self.url+method, params=params)
+        return response
+
+
+    def get_cards(self, list_id):
+        method = f'lists/{list_id}/cards'
         params = {
             'key': self.api_key,
             'token': self.api_token,
